5c5fcb4410757b7eb04da4373f698066b58b6a95
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-04 15:33:05 +0200

    fine

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,28 @@
-const kilometri = prompt("Quanti chilometri devi percorrere?")
+const kilometri = parseInt(prompt("Quanti chilometri devi percorrere?")) ;
 console.log(kilometri);
+document.getElementById("kilometri").innerHTML = `Devo percorrere: ${kilometri}km`
+
+const eta = parseInt(prompt("Quanti anni hai?")) ;
+console.log(eta);
+document.getElementById("anni").innerHTML = `Ho ${eta} anni`
+
+
+if (eta < 18) {
+    const prezzodefinitivo = ((kilometri* 0.21)* 80/100)
+    console.log(prezzodefinitivo);
+    document.getElementById("prezzo").innerHTML = `Il costo del tuo biglietto è: ${prezzodefinitivo}$`
+} else if (eta >= 60)  {
+    const prezzodefinitivo = ((kilometri* 0.21)* 40/100)
+    console.log(prezzodefinitivo);
+    document.getElementById("prezzo").innerHTML = `Il costo del tuo biglietto è: ${prezzodefinitivo}$`
+}else {
+    const prezzodefinitivo = kilometri * 0.21
+    console.log(prezzodefinitivo);
+    document.getElementById("prezzo").innerHTML = `Il costo del tuo biglietto è: ${prezzodefinitivo}$`
+}
+
+
+
+
+
+
